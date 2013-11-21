8a0793503633f906054d39daad5fae228638c9da
Sandra <sandra@elementum.com>
2013-11-21 09:02:16 -0800

    tests added

--- SampleSuperproject/ViewController.m
+++ SampleSuperproject/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <SampleSubproject/SampleSubproject.h>
 
 @interface ViewController ()
 
@@ -18,6 +19,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"in super: %@", [[[SSHelloer alloc] init] hello]);
 }
 
 - (void)didReceiveMemoryWarning
